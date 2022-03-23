1afc7818a1272969c866e2853b7ebaee6cf39332
leejh96 <dlwngur96@naver.com>
2022-03-23 18:10:40 +0900

    update

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -14,7 +14,7 @@ router.get(
     failureRedirect: "/login",
   }),
   (req, res) => {
-    res.cookie("oauth", true).redirect("/");
+    res.cookie("oauth", true).redirect("https://julogpage.herokuapp.com");
   }
 );
 
