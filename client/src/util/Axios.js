0ee15d4273117d1bb48c26f66bccb73315a37fdb
leejh96 <dlwngur96@naver.com>
2022-03-23 17:07:00 +0900

    re deploy

--- client/src/util/Axios.js
+++ client/src/util/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = axios.create();
 
-Axios.defaults.baseURL = "http://localhost:5000";
+Axios.defaults.baseURL = "https://julogpage.herokuapp.com";
 Axios.defaults.withCredentials = true;
 
 export default Axios;
